9bef3da284d75651b58fdefc0f3433c7bee86d52
Luis Miguel Del Valle <150975497+lumi-tip@users.noreply.github.com>
2024-03-19 23:02:22 +0000

    Finished

--- src/app.py
+++ src/app.py
@@ -15,6 +15,31 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+John = {
+    "id": jackson_family._generateId(),
+    "name":"John Jackson",
+    "age": "33 Years old",
+    "lucky_numbers": [7, 13, 22]
+}
+
+Jane = {
+    "id": jackson_family._generateId(),
+    "name":"Jane Jackson",
+    "age":"35 Years old",
+    "lucky_numbers": [10, 14, 3]
+}
+
+Jimmy = {
+    "id": jackson_family._generateId(),
+    "name": "Jimmy  Jackson",
+    "age": "5 Years old",
+    "lucky_numbers": 1
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,17 +51,38 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_members():
 
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
+    response_body = members
 
     return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_members():
+    body = request.json
+    if "id" not in body:
+        body["id"] = jackson_family._generateId()
+    
+    jackson_family.add_member(body)
+    return "Family member added", 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_one_member(id):
+    member = jackson_family.get_member(id)
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+    else:
+        return jsonify(member), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_one_member(id):
+    member = jackson_family.delete_member(id)
+    if member == True:
+        return {"done": True}, 200
+    if member == False:
+        return 'member doesnt exist', 400
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
